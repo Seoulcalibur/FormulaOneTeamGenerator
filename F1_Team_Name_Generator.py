5281e1e4279acadb4b842ee0822e78efe1b82254
Seoulcalibur <144836634+Seoulcalibur@users.noreply.github.com>
2023-12-15 21:49:39 -0500

    Add files via upload

---
+++ F1_Team_Name_Generator.py
@@ -0,0 +1,18 @@
+import openai
+
+openai.api_key = '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'
+prompt = "Create a unique and catchy Formula 1 team name combining a well-known automotive brand with a globally recognized non-automotive brand. The team name should end with 'F1 Team'."
+
+def get_completion(prompt, model):
+    try:
+        response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+response = get_completion(prompt, model)
+
+print(response)
