089db3a4ef372592613981af31a1fde7285e84b9
Seoulcalibur <144836634+Seoulcalibur@users.noreply.github.com>
2023-12-15 22:50:48 -0500

    Update F1TeamNameGenerator.py

--- F1TeamNameGenerator.py
+++ F1TeamNameGenerator.py
@@ -7,30 +7,31 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 def create_team(prompt):
     try:
         response = openai.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": prompt}]
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": prompt}]
         )
         return response.choices[0].message.content
     except Exception as e:
         return f"An error occurred: {e}"
 
 def save_responses(new_teamname, filename="F1TeamNames.json"):
-     try:
+    try:
         # Read existing data from the file
         with open(filename, "r") as file:
             teamnames = json.load(file)
     except FileNotFoundError:
         # If the file doesn't exist, start with an empty list
         teamnames = []
-    # Append the new response
-    teamnames.append(new_response)
+        
+    # Append the new team name
+    teamnames.append(new_teamname)
+    
     # Write the updated list back to the file
     with open(filename, "w") as file:
         json.dump(teamnames, file)
 
 prompt = "Create a unique and catchy Formula 1 team name combining a well-known automotive car brand (full name) with a globally recognized non-automotive brand. The team name should end with 'F1 Team'."
 teamname = create_team(prompt)
-teamname = [teamname]
 save_responses(teamname)
 
 print(teamname)
