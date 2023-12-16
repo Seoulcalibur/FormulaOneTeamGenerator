d5f7636e49ef735653da22c3f76ef7caec07b40e
Seoulcalibur <144836634+Seoulcalibur@users.noreply.github.com>
2023-12-15 22:25:07 -0500

    Update F1TeamNameGenerator.py

--- F1TeamNameGenerator.py
+++ F1TeamNameGenerator.py
@@ -16,7 +16,7 @@ def create_team(prompt):
 
 def save_responses(response, filename="F1TeamNames.json"):
     with open(filename, "w") as file:
-        json.dump(responses, file)
+        json.dump(response, file)
 
 prompt = "Create a unique and catchy Formula 1 team name combining a well-known automotive car brand (full name) with a globally recognized non-automotive brand. The team name should end with 'F1 Team'."
 response = create_team(prompt)
