8ec2f2b8db02d8c9ee0ffa08fee32a956b02aea5
Seoulcalibur <144836634+Seoulcalibur@users.noreply.github.com>
2023-12-15 22:46:54 -0500

    Update F1TeamNameGenerator.py

--- F1TeamNameGenerator.py
+++ F1TeamNameGenerator.py
@@ -14,13 +14,23 @@ def create_team(prompt):
     except Exception as e:
         return f"An error occurred: {e}"
 
-def save_responses(response, filename="F1TeamNames.json"):
+def save_responses(new_teamname, filename="F1TeamNames.json"):
+     try:
+        # Read existing data from the file
+        with open(filename, "r") as file:
+            teamnames = json.load(file)
+    except FileNotFoundError:
+        # If the file doesn't exist, start with an empty list
+        teamnames = []
+    # Append the new response
+    teamnames.append(new_response)
+    # Write the updated list back to the file
     with open(filename, "w") as file:
-        json.dump(response, file)
+        json.dump(teamnames, file)
 
 prompt = "Create a unique and catchy Formula 1 team name combining a well-known automotive car brand (full name) with a globally recognized non-automotive brand. The team name should end with 'F1 Team'."
-response = create_team(prompt)
-response = [response]
-save_responses(response)
+teamname = create_team(prompt)
+teamname = [teamname]
+save_responses(teamname)
 
-print(response)
+print(teamname)
